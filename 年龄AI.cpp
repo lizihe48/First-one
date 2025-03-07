188fde0d8b61b4f620ed7ea4aa69c2a95be72aed
lizihe48 <167404112+lizihe48@users.noreply.github.com>
2025-03-07 21:58:17 +0800

    Add files via upload

--- 年龄AI.cpp
+++ 年龄AI.cpp
@@ -8,13 +8,15 @@ int main() {
     printf("请依次输入出生年月日\n");
     scanf_s("%d%d%d", &birthYear, &birthMonth, &birthDay);
 
-    age = currentYear - birthYear;
+	age = currentYear - birthYear;//当前年份-出生年份
 
     if (birthMonth > currentMonth || (birthMonth == currentMonth && birthDay > currentDay)) {
+		//生日>当前日期 或者 生日月份==当前月份 而且 生日日期>当前日期
         age--;
     }
 
     if (birthMonth == currentMonth && birthDay == currentDay) {
+		//生日月份==当前月份 而且 生日日期==当前日期
         printf("生日快乐！！！\n");
     }
 
