5d10484093272a1a2708bb7386be2b1437ffed59
lizihe48 <167404112+lizihe48@users.noreply.github.com>
2025-03-09 11:31:33 +0800

    Add files via upload

--- 三个整数比大小.cpp
+++ 三个整数比大小.cpp
@@ -2,25 +2,47 @@
 int main()
 {
 	int a, b, c;
+	printf("请依次输入三个数字\n");
 	scanf_s("%d %d %d", &a, &b, &c);
-	if (a > b && b > c) {
-	  printf("%d\>%d\>%d",a, b, c);
+	if (a == b && b == c) {
+		printf("%d\=%d\=%d", a, b, c);
 	}
-	  if (a > b && c > b) {
-		printf("%d\>%d\>%d", a, c, b);
+	  if (a == b && b > c) {
+		printf("%d\=%d\>%d", a, b, c);
 	  }
-	    if (b> a && a > c) {
-		  printf("%d\>%d\>%d", b, a, c);
+	    if (a == b && c > b) {
+		  printf("%d\>%d\=%d", c, a, b);
 	    }
-	      if (b> a && c > a) {
-		    printf("%d\>%d\>%d", b, c, a);
-	      }
-		    if (c > a && b > a) {
-			  printf("%d\>%d\>%d", c, b, a);
+		  if (a == c && c > b) {
+			printf("%d\=%d\>%d", a, c, b);
+		  }
+		    if (b == c && a > b) {
+			  printf("%d\>%d\=%d", a, b, c);
 		    }
-			  if (c > a && a > b) {
-				  printf("%d\>%d\>%d", c, a, b);
-			  }
+		      if (b == c && a > b) {
+			    printf("%d\>%d\=%d", a, b, c);
+		      }
+			    if (b == c && b > a) {
+				  printf("%d\=%d\=%d", c, b, a);
+			    }
+	               if (a > b && b > c) {
+	                 printf("%d\>%d\>%d",a, b, c);
+	               }
+	                  if (a > b && c > b&&a>c){
+		                 printf("%d\>%d\>%d", a, c, b);
+	                  }
+	                    if (b> a && a > c) {
+		                   printf("%d\>%d\>%d", b, a, c);
+	                    }
+	                      if (b> a && c > a && b>c) {
+		                    printf("%d\>%d\>%d", b, c, a);
+	                      }
+			                if (c > a && a > b) {
+				               printf("%d\>%d\>%d", c, a, b);
+			                }
+						     if (c > b && b > a && c>a) {
+								  printf("%d\>%d\>%d", c, b, a);
+					         }
 
 	return 0;
 }
